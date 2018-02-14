884663555794eceb6c6afdab2635806921778329
Jason Borer <borerjason@gmail.com>
2018-02-13 16:12:36 -0800

    add javascsript clock

--- takeaways.js
+++ takeaways.js
@@ -10,3 +10,10 @@ window.addEventListener('keydown', playSound);
 
 // use transitionend to listen for the end of a transition
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// use transform-orgin to control where the transform pivot takes place
+transform - origin: 100%;
+
+// get current time and seconds
+const date = new Date();
+const seconds = date.getSeconds();
